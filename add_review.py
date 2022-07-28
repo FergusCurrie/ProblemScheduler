9c84d49e97c163cbba88c7305b419be300d0dbf7
Fergus Currie <ferguscurrie@Ferguss-MacBook-Pro.local>
2022-07-28 19:18:40 +1200

    added review dates and some checks on generation + fist reviews

--- add_review.py
+++ add_review.py
@@ -1,10 +1,13 @@
 import pandas as pd 
 import numpy as np
+import datetime
+
 
 book = input("Reviewed Book: ")
 chapter = input("Reviewed Chapter: ")
 question = input("Reviewed Question: ")
 
+x = datetime.datetime.now()
 
 df = pd.read_csv('questions.csv')
 
@@ -15,4 +18,5 @@ dff = dff[dff.question_number == float(question)]
 index = dff.index[0]
 
 df.loc[index, 'reviewed'] = 1
+df.loc[index, 'review_date'] = x
 df.to_csv('questions.csv', index = False)
--- generator.py
+++ generator.py
@@ -13,17 +13,31 @@ num_questions = input('Number questoins:')
 
 columns = ['book', 'chapter', 'question_number', 'reviewed', 'review_date']
 
-# Make df2 
-data = []
-for i in range(int(num_questions)):
-    print(i)
-    data.append([book, chapter, i+1, 0, None])
-
-df2 = pd.DataFrame(data=data, columns=columns)
-
+# Add a check on book name 
 if os.path.exists('questions.csv'):
     df = pd.read_csv('questions.csv')
+
+create = True
+if not book in pd.unique(df.book):
+    create = False
+    ans = input("This is a new book, create (y/n)? ")
+    if ans == 'y':
+        create = True
+    if not create:
+        print("Ok not creating a new book")
+
+if create:
+
+    # Make df2 
+    data = []
+    for i in range(int(num_questions)):
+        print(i)
+        data.append([book, chapter, i+1, 0, None])
+
+    df2 = pd.DataFrame(data=data, columns=columns)
+
+    # Append columns
     df2 = df2.append(df, ignore_index = True)
 
-print(df.columns == columns)
-df2.to_csv('questions.csv', index = False)
+    print(df.columns == columns)
+    df2.to_csv('questions.csv', index = False)
